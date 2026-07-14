0ec93fc85a7a22c1c9c9d6877f16e434ce34ca28
Jorge Jiménez (Jota) <42505799+JotaZonar@users.noreply.github.com>
2026-07-14 16:41:27 -0400

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 VERSION=0.01
-DESTINATION=docker.zonar.cl/test-zd/backend
+DESTINATION=docker.homejota.net/eq/backend
 docker buildx build --push --platform linux/amd64,linux/arm64 -t $DESTINATION:latest -t $DESTINATION:$VERSION .
